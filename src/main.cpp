572aafdf7ac7001ba9863fc6b1bda96d56d536c4
DinoMax00 <527663226@qq.com>
2022-02-13 22:24:36 +0800

    finish all

--- src/main.cpp
+++ src/main.cpp
@@ -28,35 +28,71 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+// random generate some different balls
+hittable_list random_scene() {
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = inner_mult(color::random(), color::random());
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    world.add(make_shared<sphere>(point(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 int main() {
 
     /// Image
     constexpr double aspect_ratio = 16.0 / 9.0;
-    constexpr int image_width = 400;
+    constexpr int image_width = 1200;
     constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
     constexpr int samples_per_pixel = 100;
     constexpr int max_depth = 50;
 
     /// World
-    hittable_list world;
+    hittable_list world = random_scene();
     
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
-
-    world.add(make_shared<sphere>(point( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point(-1.0,    0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<sphere>(point( 1.0,    0.0, -1.0),   0.5, material_right));
-
     /// camera
-    point lookfrom(3, 3, 2);
-    point lookat(0, 0, -1);
-    vec3 vup(0, 1, 0);
-    auto dist_to_focus = (lookfrom - lookat).length();
-    auto aperture = 2.0;
+    point lookfrom(13,2,3);
+    point lookat(0,0,0);
+    vec3 vup(0,1,0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
 
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
@@ -64,7 +100,7 @@ int main() {
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
-        if (j % 32 == 0) std::cerr << "Drawing row: " << j << std::endl;
+        std::cerr << "Drawing row: " << j << std::endl;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < samples_per_pixel; ++s) {
